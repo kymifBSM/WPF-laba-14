1475188a0e99bef34e71e58421def91e487f9296
kymifBSM <sandmanfin@gmail.com>
2023-05-25 10:03:58 +0700

    2 tasks

---
+++ task3Slider.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_laba14
+{
+    /// <summary>
+    /// Логика взаимодействия для task3Slider.xaml
+    /// </summary>
+    public partial class task3Slider : Window
+    {
+        public task3Slider()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void SliderValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            ((Slider)sender).SelectionEnd = e.NewValue;
+
+        }
+    }
+}
